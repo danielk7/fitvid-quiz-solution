54d9e640e955facd653e3cca76e1f95c4822a955
danielk7 <dkovalyov@engr-134-07.eastern.ewu.edu>
2014-10-13 13:28:30 -0700

    Version 0

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+$("#thing-with-videos").fitVids({ ignore: '.container, #myviiid'});
